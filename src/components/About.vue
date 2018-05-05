6a3005d3ffb4674f28aa34beb6c4d91ba07f6378
jgatbonton <judd.gatbonton@gmail.com>
2018-05-05 13:32:43 -0700

    First commit

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>Hello {{ their_name }}, This is my Vue Practice</h1>
+    <p>This framework is pretty cool.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Skills',
+    data() {
+      return {
+          their_name: this.$route.params.name
+      }
+    }
+  }
+</script>
